d62c0ed6212474fdeaef4f417a4e160e1aa71b7e
vlukyane <vlukyane@yandex.ru>
2018-04-05 23:07:57 +0300

    Update WorkWithDB.py

--- WorkWithDB.py
+++ WorkWithDB.py
@@ -1,7 +1,7 @@
 import csv
+import config
 
-
-class ProcessingRequest():
+class ProcessingRequest:
 
     def __init__(self):
         self.query_part_number = ''
@@ -13,18 +13,33 @@ class ProcessingRequest():
         self.query_part_number = part_number
 
     def dictation_preprocess(self):
-        print('kek')
         self.detail_dict = {}
-        with open('/home/vukyane/Pytin/bot/filefoldr/test.tsv') as tsvFile:
+        with open(config.file_location) as tsvFile:
             reader = csv.DictReader(tsvFile, dialect='excel-tab')
             for row in reader:
-                the_next_detail_pt_num = row['part_number']
+                the_next_detail_pt_num = row['part_number'].lower()
                 if the_next_detail_pt_num in self.detail_dict:
-                    self.add_to_dict(the_next_detail_pt_num, [row['supplier'], float(row['price'])])
+                    self.add_to_dict(the_next_detail_pt_num,
+                                     [row['supplier'],
+                                      float(row['price']),
+                                      row['brand'],
+                                      int(row['quantity']),
+                                      int(row['case_quantity']),
+                                      float(row['shipping_weight']),
+                                      row['handling_time'],
+                                      row['image']])
 
                 else:
                     self.detail_dict[the_next_detail_pt_num] = list()
-                    self.add_to_dict(the_next_detail_pt_num, [row['supplier'], float(row['price'])])
+                    self.add_to_dict(the_next_detail_pt_num,
+                                     [row['supplier'],
+                                      float(row['price']),
+                                      row['brand'],
+                                      int(row['quantity']),
+                                      int(row['case_quantity']),
+                                      float(row['shipping_weight']),
+                                      row['handling_time'],
+                                      row['image']])
 
     def add_to_dict(self, key, det_info):
         self.detail_dict[key].append(det_info)
@@ -32,6 +47,6 @@ class ProcessingRequest():
     def data_proc(self):
         self.ans = self.detail_dict.get(self.query_part_number, 'empty')
         if self.ans != 'empty':
-            return sorted(self.ans, key=lambda i: i[1], reverse=False)
+            return sorted(self.ans, key=lambda i: [i[2].lower(), i[1]])
         else:
             return 'Sorry, there is no given part number :('
